cee7d8ace36cf410a2ce521eb229a7bf66c3200d
Divijadiwan <79202293+Divijadiwan@users.noreply.github.com>
2021-05-04 15:07:28 +0530

    Add files via upload

---
+++ connect.js
@@ -0,0 +1,31 @@
+class connect { 
+    constructor(bodyA, pointB){ 
+        var options = 
+        { bodyA : bodyA,
+             pointB : pointB, 
+             stiffness: 0.04, length: 20, }
+              this.pointB = pointB; 
+              this.connect = Constraint.create(options); 
+              World.add(world, this.connect); 
+            } 
+              attach(body)
+              { 
+                  this.connect.bodyA = body;
+             } 
+             fly()
+                {
+                    this.connect.bodyA = null;
+                } 
+             
+              display()
+              { 
+                  if(this.connect.bodyA )
+                  { var pointA = this.connect.bodyA.position; 
+                    var pointB = this.pointB; strokeWeight(4); 
+                    line(pointA.x, pointA.y, pointB.x, pointB.y);
+                 } 
+                }
+                
+                
+}
+                
